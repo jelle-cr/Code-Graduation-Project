eb8985c17fc50e3d93193445c2478074c2cb8cf9
jelle-cr <126500480+jelle-cr@users.noreply.github.com>
2024-04-28 13:44:40 +0200

    Added collision avoidance according to (13) of Multiagent Systems (Jankovic)
    Calculations done in notebook.
    #######################
    Persisting bug when N_a is 3, values tend to go to infinity.
    Suspected cause numerical errors.
    #######################

--- Distributed CBF/odefcn.m
+++ Distributed CBF/odefcn.m
@@ -1,19 +1,17 @@
 %% Second order (added nominal trajectory)
-function dpdt = odefcn(t,p_combined)      
+function dpdt = odefcn(t,p)      
     global u_save u_nom
 
     load('parameters.mat');
     
-    p_combined = reshape(p_combined, height(p_combined)/N_a, N_a)
-    p = p_combined(1:states,:)
-    p0 = p_combined(states+1:2*states,:)
-    dpdt = zeros(2*height(p),N_a);
+    p = reshape(p, states, N_a);
+    dpdt = zeros(states, N_a);
     % Note that [p(1) p(2) p(3) p(4)]==[x y x' y']
     % time
     t
 
     % Full responsibility mu = 1, or half responsibility mu = 1/2
-    mu = 1;
+    mu = 1/2;
 
     % Nominal controls
     u0 = sign_rand.*[A_rand; A_rand].*[cos(f_rand*t); sin(f_rand*t)];
@@ -26,15 +24,13 @@ function dpdt = odefcn(t,p_combined)
         for j = 1:N_a
             if i ~= j
                 xi_ij = p(1 : dimensions, i) - p(1 : dimensions, j);
-                % v_ij = p(dimensions+1 : 2*dimensions, i) - p(dimensions+1 : 2*dimensions, j);
+                v_ij = p(dimensions+1 : 2*dimensions, i) - p(dimensions+1 : 2*dimensions, j);
                 h = xi_ij.'*xi_ij - (2*agent_radius)^2;
-                grad_h = [2*xi_ij; 0; 0];
-                L_fh = 2*[p(3,i), p(4,i)]*xi_ij;
-                L_f2h = 2*p(3,i)^2 + 2*p(4,i)^2 - 2*d/m*xi_ij.'*[p(3,i); p(4,i)];
-                L_gL_fh = 2/m*xi_ij.';
+                a_ij = 2*v_ij.'*v_ij + 2*(l1-d/m)*xi_ij.'*v_ij + l0*h;
+                b_ij = 2*1/m*xi_ij.';
 
-                A = [A; -L_gL_fh];
-                b = [b; L_f2h + l1*L_fh + l0*h];
+                A = [A; -b_ij];
+                b = [b; mu*a_ij];
             end
         end
         H = 2*eye(dimensions);
@@ -51,12 +47,5 @@ function dpdt = odefcn(t,p_combined)
     dpdt(2,:) = p(4,:);
     dpdt(3,:) = -d/m*p(3,:) + 1/m * u(1,:);
     dpdt(4,:) = -d/m*p(4,:) + 1/m * u(2,:);
-    % Nominal trajectory
-    dpdt(5,:) = p0(3,:);                           
-    dpdt(6,:) = p0(4,:);
-    dpdt(7,:) = -d/m*p0(3,:) + 1/m * u0(1,:);
-    dpdt(8,:) = -d/m*p0(4,:) + 1/m * u0(2,:);
-    dpdt
     dpdt = reshape(dpdt, [], 1);
-    dpdt
 end
