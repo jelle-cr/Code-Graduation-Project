aed3224985d75e468aa4318c2a25997c5348c9f8
jelle-cr <126500480+jelle-cr@users.noreply.github.com>
2024-08-24 09:50:15 +0200

    Potential field analysis APF, before adding dynamics

--- Potential Field Analysis/Matlab Code/APFanalysis.m
+++ Potential Field Analysis/Matlab Code/APFanalysis.m
@@ -6,7 +6,7 @@ x = linspace(-3,3,n);
 y = linspace(-3,3,n);
 
 r_a = 0.5;
-r_o = 0.5;
+r_o = 0.4;
 
 K_att = 1;
 N_a = 1;
@@ -16,7 +16,7 @@ F_att = zeros(length(x), length(y), height(p_d));
 
 K_rep = 0.001;
 rho_0 = 0.5;
-N_o = 5;
+N_o = 15;
 p_o = rand(2,N_o)*(3+3)-3;
 U_rep = zeros(length(x), length(y));
 F_rep = zeros(length(x), length(y), height(p_d));
@@ -28,7 +28,7 @@ for i = 1:length(x)
         F_att(i,j,:) = K_att*(p - p_d);
 
         for o = 1:N_o
-            rho = norm(p - p_o(:,o)) - 2*r_a;   
+            rho = norm(p - p_o(:,o)) - r_a - r_o;   
             if rho < rho_0
                 rho = max(rho, 1e-6);           % In order to fill in the cylinder
                 U_rep(i,j) = U_rep(i,j) + 1/2*K_rep*(1/rho - 1/rho_0)^2;
@@ -47,7 +47,19 @@ Potential = min(Potential, 1*max(max(U_att)));
 Force = -F_att - F_rep;
 
 minPotential = min(min(Potential));
-[globalMinX, globalMinY] = find(Potential == minPotential);
+[globalMinX_idx, globalMinY_idx] = find(Potential == minPotential);
+globalMinX = x(globalMinX_idx);
+globalMinY = y(globalMinY_idx);
+
+% Find local minima using imregionalmin
+localMinima = imregionalmin(Potential);     % Way faster than looping through matrix manually
+
+% Get the indices of the local minima
+[localMinX_idx, localMinY_idx] = find(localMinima);
+
+% Convert indices to coordinates in the x and y arrays
+localMinX = x(localMinX_idx);
+localMinY = y(localMinY_idx);
 
 %% Plotting
 % Surface plot
@@ -82,11 +94,26 @@ for o = 1:N_o
     patch(x_circle, y_circle, 'black', 'HandleVisibility', 'off');
 end
 
+% Plot Local Minima
+for i = 1:length(localMinX)
+    if abs(localMinX(i) - globalMinX) > 1e-6 || abs(localMinY(i) - globalMinY) > 1e-6      % Exclude the Global Minimum
+        plot(localMinX(i), localMinY(i), 'o', 'MarkerSize', 8, ...
+                                 'MarkerEdgeColor', 'green', ...
+                                 'MarkerFaceColor', 'green', ...
+                                 'HandleVisibility', 'off');
+    else
+        plot(NaN, NaN, 'o', 'MarkerEdgeColor', 'green', ...
+                            'MarkerFaceColor', 'green', ...
+                        	'MarkerSize', 8, ...
+                            'DisplayName', 'Local Minimum');
+    end
+end
+
 % Plot Global minimum
-plot(x(globalMinX), y(globalMinY), 'x','MarkerSize', 12, ...
-                         'MarkerEdgeColor', 'blue', ...
-                         'LineWidth', 2,...
-                         'DisplayName', 'Global Minimum');
+plot(globalMinX, globalMinY, 'o', 'MarkerSize', 8, ...
+                                        'MarkerEdgeColor', 'blue', ...
+                                        'MarkerFaceColor', 'blue', ...
+                                        'DisplayName', 'Global Minimum');
 
 % Plot Goal
 plot(p_d(1), p_d(2), 'x','MarkerSize', 12, ...
